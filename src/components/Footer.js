97bae9b6f74e59e6be68b86ac9d60ba9d3ed0cd2
Andrew VanNess <avanness@student.bridgew.edu>
2019-02-21 23:40:30 -0500

    add kofi button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,13 +13,19 @@ function Footer() {
           Oftheheadland
         </a>
       </p>
+
       <a
-        href="https://ko-fi.com/lunith"
-        target="_blank"
+        href="https://ko-fi.com/E1E3LTW5"
         rel="noopener noreferrer"
-        className="snapper-button donate-button"
+        target="_blank"
       >
-        Donate
+        <img
+          height="36"
+          className="kofi-button"
+          src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
       </a>
     </div>
   );
